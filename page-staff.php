2227c62177b4c2048efd90528d9418ca8d5b3c1f
kristinahunt <kristinahunt@shaw.ca>
2023-05-26 11:49:57 -0700

    registered cpt and taxonomy terms

--- page-staff.php
+++ page-staff.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying staff
  *
- * This is the template that displays all pages by default.
+ * This is the template that displays the staff taxonomy by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
