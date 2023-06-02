24c842448455562969d72be860ff4f5f2f9e228e
kristinahunt <kristinahunt@shaw.ca>
2023-06-02 15:14:51 -0700

    home changes

--- header.php
+++ header.php
@@ -29,15 +29,6 @@
 		<div class="site-branding">
 			<h1><?php the_custom_logo(); ?></h1>
 			<?php
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
 			$mchigh_description = get_bloginfo( 'description', 'display' );
 			if ( $mchigh_description || is_customize_preview() ) :
 				?>
